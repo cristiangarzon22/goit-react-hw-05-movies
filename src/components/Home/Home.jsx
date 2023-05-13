3a821ed7f6c40e37a6a43cc36ad7638418499e77
cristiangarzon22 <86244196+cristiangarzon22@users.noreply.github.com>
2023-05-13 12:35:27 -0500

    new commit

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   const [data, setData] = useState(null);
@@ -24,12 +25,15 @@ const Home = () => {
     <div>
       <h1>Home</h1>
       {data && (
-        <ul>
-          {data.map((e) => (
-            <li key={e.id}>{e.title}</li>
-          ))}
-        </ul>
-      )}
+  <ul>
+    {data.map((e) => (
+      <li key={e.id}>
+        <Link to={`/movies/${e.id}`}>{e.title}</Link>
+      </li>
+    ))}
+  </ul>
+)}
+
     </div>
   );
 };
--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
-const MoviesDetails = () => {
+const MovieDetails = () => {
+  const { id: movieId } = useParams(); 
+  const [movie, setMovie] = useState(null);
+
+  useEffect(() => {
+    async function fetchMovieData() {
+      try {
+        const response = await fetch(
+          `https://api.themoviedb.org/3/movie/${movieId}?api_key=ce046fb7a0718a1aac652aa1ca1238c4`
+        );
+        const data = await response.json();
+        setMovie(data);
+        
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    fetchMovieData();
+  }, [movieId]); 
+
+  if (!movie) {
+    return <div>Loading...</div>;
+  }
+  console.log(movie);
   return (
     <div>
-      <h2>Movie Deatails</h2>
+       console.log(movie.title);
+      <h1>{movie.title}</h1>
+      <p>{movie.overview}</p>
+      <img
+        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+        alt={`Poster for ${movie.title}`}
+      />
     </div>
   );
-}
+};
 
-export default MoviesDetails;
+export default MovieDetails;
