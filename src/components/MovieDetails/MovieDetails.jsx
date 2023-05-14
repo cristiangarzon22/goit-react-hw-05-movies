c90f5694be62e6f4edcc2a181d60b003b75bbd0e
cristiangarzon22 <86244196+cristiangarzon22@users.noreply.github.com>
2023-05-13 19:29:41 -0500

    new commit

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import css from "components/MovieDetails/MovieDetails.module.css";
+import { useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const MovieDetails = () => {
   const { id } = useParams(); 
   const [movie, setMovie] = useState(null);
-
+  const location = useLocation();
+  const cameBack = location.state?.from ??'/';
+  
   useEffect(() => {
     async function fetchMovieData() {
       try {
@@ -29,7 +33,7 @@ const MovieDetails = () => {
   console.log(movie);
   return (
     <>
-    <button className={css.btn}> {'<-'} Go Back</button>
+    <button className={css.btn}><Link to={cameBack}>{'<-'} Go Back</Link></button>
     <div className={css.container}>
       
       <div className={css.picture}>
@@ -42,12 +46,22 @@ const MovieDetails = () => {
       </div>
       <div className={css.info}>
       <h1>{movie.title} ({movie.release_date.substring(0, 4)})</h1>
+      <p>user score:{(movie.vote_average*10)}%</p>
       <h2>overview</h2>
       <p>{movie.overview}</p>
       <h2>generos</h2>
       <p>{movie.genres.map(genre => genre.name).join(', ')}</p>
 
       </div>
+    </div>
+    <div>
+    <Link to="cast" state={{ from: cameBack }}>
+  <button>Cast</button>
+</Link>
+
+<Link to="reviews" state={{ from: cameBack }}>
+  <button>Reviews</button>
+</Link>
     </div>
     </>
   );
